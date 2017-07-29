b1d83051890222563852887d865509951f7e4d4c
Kirschn <github@kirschn.de>
2017-07-30 01:43:25 +0200

    fugg

--- kue-discord-reader.js
+++ kue-discord-reader.js
@@ -14,20 +14,41 @@ var kue = require('kue'),
             host: '127.0.0.1'
         }
     });
-function censor(censor) {
-    var i = 0;
+function resolveReferences(json) {
+    if (typeof json === 'string')
+        json = JSON.parse(json);
 
-    return function(key, value) {
-        if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value)
-            return '[Circular]';
+    var byid = {}, // all objects by id
+        refs = []; // references to objects that could not be resolved
+    json = (function recurse(obj, prop, parent) {
+        if (typeof obj !== 'object' || !obj) // a primitive value
+            return obj;
+        if ("$ref" in obj) { // a reference
+            var ref = obj.$ref;
+            if (ref in byid)
+                return byid[ref];
+            // else we have to make it lazy:
+            refs.push([parent, prop, ref]);
+            return;
+        } else if ("$id" in obj) {
+            var id = obj.$id;
+            delete obj.$id;
+            if ("$values" in obj) // an array
+                obj = obj.$values.map(recurse);
+            else // a plain object
+                for (var prop in obj)
+                    obj[prop] = recurse(obj[prop], prop, obj)
+            byid[id] = obj;
+        }
+        return obj;
+    })(json); // run it!
 
-        if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-            return '[Unknown]';
-
-        ++i; // so we know we aren't using the original object anymore
-
-        return value;
+    for (var i=0; i<refs.length; i++) { // resolve previously unknown references
+        var ref = refs[i];
+        ref[0][ref[1]] = byid[refs[2]];
+        // Notice that this throws if you put in a reference at top-level
     }
+    return json;
 }
 var triggerWords = {
 
@@ -67,10 +88,7 @@ function biotmp(token) {
         };
         var job = queue.create('msg_process', {
             "title": "Discord Message Processing",
-            "message-obj": JSON.stringify(elMSGObj, function( key, value) {
-            if( key == 'parent') { return value.id;}
-            else {return value;}
-        })
+            "message-obj": JSON.stringify(resolveReferences(elMSGObj))
 
         }).priority("low").save( function(err){
             if( !err ) console.log( job.id );
